c03eefb6b9434977333b4af6e2bdfd9c1b561929
ravelll <ravelll.taniguchi@gmail.com>
2016-11-09 19:00:15 +0900

    click extension icon and remove duplicated tabs

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,5 +4,24 @@ var queryInfo = {
   windowId: chrome.windows.WINDOW_ID_CURRENT
 };
 
-chrome.tabs.query(queryInfo, tabs => {
+chrome.browserAction.onClicked.addListener(tabs => {
+  chrome.tabs.query(queryInfo, tabs => {
+    tabs.sort(function (tab1, tab2) {
+      if (tab1.url > tab2.url) {
+        return -1;
+      }
+
+      if (tab1.url < tab2.url) {
+        return 1;
+      }
+
+      return 0;
+    })
+
+    for (var i = 0; i < tabs.length - 1; ++i) {
+      if (tabs[i].url == tabs[i + 1].url) {
+        chrome.tabs.remove(tabs[i + 1].id);
+      }
+    }
+  });
 });
