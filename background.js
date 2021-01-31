3ab5078f20bc18d533db161f49989e1d7b22b8bd
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2021-01-31 17:18:34 +0900

    Fix scripts to use the manifest version 2

--- background.js
+++ background.js
@@ -1,27 +1,13 @@
-'use strict';
-
-var queryInfo = {
+const queryInfo = {
   windowId: chrome.windows.WINDOW_ID_CURRENT
-};
+}
 
-chrome.browserAction.onClicked.addListener(tabs => {
-  chrome.tabs.query(queryInfo, tabs => {
-    tabs.sort(function (tab1, tab2) {
-      if (tab1.url > tab2.url) {
-        return -1;
-      }
-
-      if (tab1.url < tab2.url) {
-        return 1;
-      }
-
-      return 0;
-    })
-
-    for (var i = 0; i < tabs.length - 1; ++i) {
-      if (tabs[i].url == tabs[i + 1].url) {
-        chrome.tabs.remove(tabs[i + 1].id);
+chrome.browserAction.onClicked.addListener(async (_) => {
+  await chrome.tabs.query(queryInfo, (tabs) => {
+    for (let i = 0; i < tabs.length - 1; ++i) {
+      if (tabs[i].url === tabs[i + 1].url) {
+        chrome.tabs.remove(tabs[i + 1].id)
       }
     }
-  });
-});
+  })
+})
